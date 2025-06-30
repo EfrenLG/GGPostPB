c9e105c0bdfe022e6ec329233c30350b1806c5f4
EfrenLG <efrenlozanoguerrero@gmail.com>
2025-06-30 10:32:27 +0200

    subida del back

--- src/app.js
+++ src/app.js
@@ -20,8 +20,18 @@ app.use(cookieParser());
 app.use(express.json());
 
 //PARA QUE NO ME BORRE EL TOKEN DE LAS COOKIES LA BASE DE LA URL TIENE QUE SER ASI!!!--> http://localhost:5500/
+const allowedOrigins = [
+  'https://gg-post-f.vercel.app',
+  'http://localhost:5173'
+];
 app.use(cors({
-  origin: 'https://gg-post-f.vercel.app',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
