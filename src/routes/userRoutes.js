aebfc5705a598fa2edbaa3cdb35093fcabd78a90
EfrenLG <efrenlozanoguerrero@gmail.com>
2026-06-18 11:10:12 +0200

    red social

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,12 +6,16 @@ const userController = require('../controllers/userController');
 router.get('/all', userController.getUsersController);
 router.get('/profile/:id', userController.getPublicProfileController); // NUEVO: perfil público
 router.get('/follow-list/:id', userController.getFollowListController); // NUEVO: lista seguidores/seguidos
+router.get('/follow-requests', userController.getFollowRequestsController); // NUEVO: solicitudes pendientes
 router.get('/:id', userController.getUserController);
 
 // POST
-router.post('/follow/:id', userController.followUserController); // NUEVO: follow/unfollow
+router.post('/follow/:id', userController.followUserController); // NUEVO: follow/unfollow/request
+router.post('/follow-requests/:id/accept', userController.acceptFollowRequestController); // NUEVO
+router.post('/follow-requests/:id/reject', userController.rejectFollowRequestController); // NUEVO
 
 // PUT
 router.put('/icon', userController.updateUserIcon);
+router.put('/privacy', userController.togglePrivateController); // NUEVO: toggle cuenta privada
 
 module.exports = router;
